85fecb22d389034f38b8edf460fb38ca1c4c0022
conductor <conductor85@gmail.com>
2025-01-14 14:50:15 +0300

    Add Install-MSOffice2016

---
+++ Scripts/Install-MSOffice2016VL.ps1
@@ -0,0 +1,7 @@
+### MS Office 2016 VL install ###
+
+Write-Verbose -Message "Installing Office 2016 VL" -Verbose
+
+Start-Process 'w:\soft\ms_office\office2016_64_vl\setup.exe' -Wait
+
+Write-Verbose -Message 'OK' -Verbose
