d49293f840047abbd0de520a4b6d3440fe3a85bc
conductor <conductor85@gmail.com>
2025-01-17 14:23:23 +0300

    Add Publish-1CBaseTOFTP and backuptools fixes

--- Modules/BackupTools.psm1
+++ Modules/BackupTools.psm1
@@ -1,18 +1,22 @@
-$BackupSettings = Import-PowerShellDataFile -Path W:\scr\Config\BackupSettings.psd1
+# $BackupSettings = Import-PowerShellDataFile -Path W:\scr\Config\BackupSettings.psd1
 
 function Export-1CBase {
+    [CmdletBinding()]
+    param (
+        [string]$Server1CName,
+        [string]$Base1CName,
+        [string]$PathTo1C,
+        [string]$DTPath,
+        [string]$DTFile
+    )
+    <#
+    .DESCRIPTION
+    This function starts 1C Enterprise in designer mode and exports database
+    to .dt file on disk
+    #>
 
-    $Settings = $BackupSettings
-
-    $1CServerName  = $Settings.ServerName
-    $1CBaseName    = $Settings.BaseName
-    $1CPath        = $Settings.PathTo1C
-    $DTPath        = $Settings.DumpPath
-    # $Login         = $Settings.Login
-    # $Password      = $Settings.Password
-
-    $DTFile        = "$BaseName" + "_" + (Get-Date -Format "yyyy-MM-dd_HHmmss") + ".dt"
-    $Arguments     = "CONFIG /S$1CServerName\$1CBaseName /DisableStartupMessages /DumpIB $DTPath\$DTFile"
+    # $DTFile        = "$Base1CName" + "_" + (Get-Date -Format "yyyy-MM-dd") + ".dt"
+    $Arguments     = "CONFIG /S$Server1CName\$Base1CName /DisableStartupMessages /DumpIB $DTPath\$DTFile"
 
     # Ensure $DTPath exists
     if (-Not (Test-Path -Path $DTPath)) {
@@ -21,25 +25,57 @@ function Export-1CBase {
         Write-Host "The path '$DTPath' has been created." -ForegroundColor Green
     }
 
-    try {
-        # Attempt to start the process
-        Start-Process -FilePath "$1CPath\1cv8.exe" -ArgumentList $Arguments -ErrorAction Stop
-        Write-Host "Process started successfully. The file will be exported to $DTPath\$DTFile" -ForegroundColor Green
-    } catch {
-        # Handle any errors that occur during process execution
-        Write-Error "An error occurred while starting the process: $_"
-    }
-}
-
-function FunctionName {
-
-    $Settings = $BackupSettings
-
-    $1CServerName   = $Settings.ServerName
-    $1CBaseName     = $Settings.BaseName
-    $1CBackupFolder = $Settings.BackupFolder
-
-
-    $FileToRestore      = Get-ChildItem -Path $1CBackupFolder | Sort-Object -Descending | Select-Object -First 1
+    Write-Host "Process started. The file is exporting to $DTPath\$DTFile" -ForegroundColor Green
+    $Process = Start-Process -FilePath "$PathTo1C\1cv8.exe" -ArgumentList $Arguments -ErrorAction Stop -Wait -PassThru
     
+    $ExportResult = $Process.ExitCode
+    if ($ExportResult -eq 0) {
+        Write-Output "Export successful"
+        return $Process.ExitCode
+    } else {
+        Write-Output "Export failed with exit code: $ExportResult"
+        return $Process.ExitCode
+    }
+
 }
+
+function Invoke-WinSCPUpload {
+    param (
+        [string]$WinSCPPath,
+        [string]$WinSCPLog,
+        [string]$FTPSConnectionString,
+        [string]$FileToUpload,
+        [string]$FTPPath
+    )
+
+    # Ensure output encoding is UTF-8
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+    # Define the commands
+    $PutCommand = "put $FileToUpload $FTPPath"
+
+    # Construct arguments for WinSCP
+    $arguments = @(
+        "/log=`"$WinSCPLog`"", 
+        "/ini=nul", 
+        "/command", 
+        "`"open $FTPSConnectionString`"", 
+        "`"$PutCommand`"", 
+        "exit"
+    )
+
+    # Start the WinSCP process
+    Write-Host "Process started. The file is uploading to FTP" -ForegroundColor Green
+    $Process = Start-Process -FilePath $WinSCPPath -ArgumentList $arguments -NoNewWindow -Wait -PassThru
+
+    # Return the result
+    $WinSCPResult = $Process.ExitCode
+    if ($WinSCPResult -eq 0) {
+        Write-Output "Upload successful." 
+    } else {
+        Write-Output "Upload failed with exit code: $WinSCPResult"
+    }
+
+}
+
+
---
+++ Scripts/Publish-1CBaseToFTPVostok.ps1
@@ -0,0 +1,41 @@
+
+Import-Module W:\scr\Modules\BackupTools.psm1 -Force
+$Settings = Import-PowerShellDataFile W:\scr\Config\BackupSettings.psd1
+
+# $FileToRestore = Get-ChildItem -Path $Settings.SQLBackupFolder | Sort-Object -Descending | Select-Object -First 1
+# try {
+#     Restore-SqlDatabase -ServerInstance $Settings.Server1CName `
+#                         -Database $Settings.Base1CName + "_test" `
+#                         -BackupFile $FileToRestore `
+#                         -ReplaceDatabase `
+#                         -ErrorAction Stop
+#                         # -Credential $SQLCredential
+# }
+# catch {
+#     Write-Error "Restore process failed"
+# }
+
+$CurYear = get-date -Format "yy"
+$CurQrt = [math]::Ceiling((Get-Date).Month/3)
+$DTFile = "83-$CurYear-$CurQrt-МинскСЭЙФТИМ.dt"
+# $DTFile = $Settings.Base1CName + "_" + (Get-Date -Format "yyyy-MM-dd_HHmmss") + ".dt"
+
+Export-1CBase -Server1CName $Settings.Server1CName `
+                -Base1CName  $Settings.Base1CName `
+                -PathTo1C $Settings.PathTo1CExecutable `
+                -DTPath $Settings.DTPath `
+                -DTFile $DTFile
+
+
+$FileToUpload = Join-Path -Path $Settings.DTPath -ChildPath $DTFile
+
+if (! (Test-Path $FileToUpload)) {
+    throw "Backup file not found: $FileToUpload"
+}
+
+# $FTPSConnectionString = Get-Content $Settings.FTPSConnectionString
+Invoke-WinSCPUpload -WinSCPPath $Settings.WinSCPCom `
+                    -WinSCPLog $Settings.WinSCPLog `
+                    -FTPSConnectionString (Get-Content $Settings.FTPSConnectionString) `
+                    -FileToUpload $FileToUpload `
+                    -FTPPath $Settings.FTPPath    
