2c3d0ea2698a95c13d3b73598004102cc8ce9ff2
conductor85 <conductor85@gmail.com>
2025-01-19 17:27:04 +0300

    fix UWPAction

--- Scripts/Uninstall-UWPApps.ps1
+++ Scripts/Uninstall-UWPApps.ps1
@@ -5,5 +5,5 @@ $Settings = Import-PowerShellDataFile -Path "W:\scr\Config\Settings.psd1"
 # Uninstalling UWP Apps
 $UWPApps = $Settings.UWPApps
 Write-Log -Message "Starting to uninstall UWP apps"
-Uninstall-UWPApps -UWPApps $UWPApps -ErrorAction Stop
+Uninstall-UWPApps -UWPApps $UWPApps -ErrorAction SilentlyContinue
 Write-Log -Message "Finished uninstalling UWP Apps"
