4d229221ffaab9a9359d1245ee7c6cd46d34f827
conductor <conductor85@gmail.com>
2025-01-19 17:38:34 +0300

    handle exception gor get-newestasusers

--- Scripts/Get-NewestADUsers.ps1
+++ Scripts/Get-NewestADUsers.ps1
@@ -1,4 +1,11 @@
 Import-Module W:\scr\Modules\ADTools.psm1
 $Settings = Import-PowerShellDataFile -Path "W:\scr\Config\ADSettings.psd1"
 
-Get-NewestADUsers -OUList $Settings.MainUsers
+try {
+    Write-host "Getting $NumberOfNewest users" -ForegroundColor Green
+    Get-NewestADUsers -OUList $Settings.MainUsers
+    Write-Host "Done!" -ForegroundColor Green
+}
+catch {
+    Write-Error "Couldn't get any users"
+}
